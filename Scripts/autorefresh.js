0a19ed8a3a734965c2c961ca25f90fe24589aa0f
Hasan ÖKTEN <hokten@gmail.com>
2018-12-01 12:38:56 +0300

    Yeni hiyerarşiye geçildi

--- Scripts/autorefresh.js
+++ Scripts/autorefresh.js
@@ -1,89 +1,90 @@
 
 const url = "/sinavsaatlerijson.aspx";
 const vm = new Vue({
-	el: '#app', 
-	
+	el: '#app',
+
 	data: {
-          results: [],
-		interval:null
-        },
-	beforeUpdate:function () {
+		results: [],
+		interval: null
+	},
+	beforeUpdate: function () {
 		console.log("before updated");
-		if(typeof $(this.$el).slick  !== 'undefined') {
-				$("#basari").show();
-				$(this.$el).slick('unslick');
+		if (typeof $(this.$el).slick !== 'undefined') {
+			$(this.$el).slick('unslick');
 		}
 	},
 	updated: function () {
 		var self = this;
-  		this.$nextTick(function () {
+		this.$nextTick(function () {
 			console.log("updated");
-			if(typeof $(self.$el).slick  !== 'undefined') {
-				setTimeout(() => {$(self.$el).slick('refresh'); $("#basari").hide(); },2000);
+			if (typeof $(self.$el).slick !== 'undefined') {
+				$("#basari").show();
+
+				setTimeout(() => { $(self.$el).slick('refresh'); $("#basari").hide(); }, 3000);
 			}
 		});
 	},
-	
+
 	mounted: function () {
 		console.log("mounted");
-		var self=this;
+		var self = this;
 		$("#basari").hide();
-  this.$nextTick(function () {
-	  $(self.$el).slick({
-  						slidesToShow: 2,
-					  slidesToScroll: 2,
-  					autoplay: true,
-  					autoplaySpeed: 2000,
-	  });
-	  /*
-	  setTimeout(function() {
-	  $(self.$el).slick({
-  						slidesToShow: 2,
-					  slidesToScroll: 2,
-  					autoplay: true,
-  					autoplaySpeed: 2000,
-				});
-	  },2000);
-*/
-			
-	  
-	  
-	  
-  });
-},
-	
-	methods :  {
-		fetchData : function(event) {
-          axios.get(url).then(response => {
-			console.log(response);
-            this.results = response.data.items;
-			
-			
-          })
-        },
-	salonParcala : function(salonlar) {
-		      	return salonlar.split(',');
+		this.$nextTick(function () {
+			$(self.$el).slick({
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				autoplay: true,
+				autoplaySpeed: 2000,
+			});
+			/*
+			setTimeout(function() {
+			$(self.$el).slick({
+								  slidesToShow: 2,
+							slidesToScroll: 2,
+							  autoplay: true,
+							  autoplaySpeed: 2000,
+					  });
+			},2000);
+	  */
+
+
+
+
+		});
+	},
+
+	methods: {
+		fetchData: function (event) {
+			axios.get(url).then(response => {
+				console.log(response);
+				this.results = response.data.items;
+
+
+			})
+		},
+		salonParcala: function (salonlar) {
+			return salonlar.split(',');
+		}
+
+	},
+	created: function () {
+		var self = this;
+		self.fetchData();
+		this.interval = setInterval(function () {
+			self.fetchData();
+		}, 30000);
+	},
+
+	beforeDestroy: function () {
+		clearInterval(this.interval);
 	}
 
-},
-					  created: function () {
-	var self = this;
-	self.fetchData();
-    				this.interval = setInterval(function () {
-      self.fetchData();
-    }, 10000); 
-  },
-
- beforeDestroy: function(){
-clearInterval(this.interval);
-}
 
 
 
-					  
-					  
-					  
-            });
 
 
-	
+});
+
+
+
